04824aff8db8beed623a37d6702bce2e163594ea
Hans <hi@hans0805.me>
2025-12-04 01:32:47 +0800

    rename

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "Hans's API",
+      title: "Hans's API Docs",
     },
   };
 }
